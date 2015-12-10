dba52192f8acf065a99a11c11ae3a243af0ce863
walk1ng <iliwgg@gmail.com>
2015-12-10 16:50:07 +0800

    using @tornado.web.authenticated and parse config file to options

--- twitter_auth/twitter.py
+++ twitter_auth/twitter.py
@@ -1,3 +1,11 @@
+"""
+  Create a file called "secrets.cfg" and put your consumer key and
+     secret (which Twitter gives you when you register an app) in it:
+       twitter_consumer_key = 'asdf1234'
+       twitter_consumer_secret = 'qwer5678'
+     (you could also generate a random value for "cookie_secret" and put it
+     in the same file, although it's not necessary to run this demo)
+"""
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
@@ -5,55 +13,56 @@ import tornado.options
 import tornado.auth
 import tornado.gen
 import os.path
+from tornado.escape import json_decode, json_encode
+import logging
 
 from tornado.options import define,options
 define('port',default=8088, help='run on the given port', type=int)
+define('config_file',default='secrets.cfg',help='filename for addtional configurations')
+define('debug',default=False,help='run app on debug mode',type=bool,group='application')
+define('twitter_consumer_key',type=str,group='application')
+define('twitter_consumer_secret',type=str,group='application')
+define('cookie_secret',type=str,group='application')
 
-class TwitterHandler(tornado.web.RequestHandler, tornado.auth.TwitterMixin):
-	@tornado.gen.coroutine
-	def get(self):
-		oAuthToken = self.get_secure_cookie('oauth_token')
-		oAuthSecret = self.get_secure_cookie('oauth_secret')
-		userID = self.get_secure_cookie('user_id')
-		
-		if oAuthToken and oAuthSecret:
-			accessToken = {
-				'key':oAuthToken,
-				'secret':oAuthSecret
-			}
-			user = yield self.twitter_request(
-				'/users/show',
-				access_token=accessToken,
-				user_id=userID,	
-			)
-			if not user:
-				self.clear_all_cookies()
-				raise tornado.web.HTTPError(500,'Could not retrieve user information')
-			self.render('home.html',user=user)
+class BaseHandler(tornado.web.RequestHandler):
+	COOKIE_NAME = 'demo_user'
+	def get_current_user(self):
+		user_json = self.get_secure_cookie(self.COOKIE_NAME)
+		if not user_json:
+			return None
 		else:
-			print 'need authorize'
-			yield self.authorize_redirect()
+			return json_decode(user_json)
+
+class TwitterHandler(BaseHandler, tornado.auth.TwitterMixin):
+	@tornado.web.authenticated
+	@tornado.gen.coroutine
+	def get(self):	
+		user = yield self.twitter_request(
+			'/users/show',
+			access_token=self.current_user['access_token'],
+			user_id = self.current_user['id']
+		)
+		if not user:
+			self.clear_all_cookies()
+			raise tornado.web.HTTPError(500,'Could not retrieve user information')
+		self.render('home.html',user=user)
 			
-class LoginHandler(tornado.web.RequestHandler,tornado.auth.TwitterMixin):
+class LoginHandler(BaseHandler,tornado.auth.TwitterMixin):
 	@tornado.gen.coroutine
 	def get(self):
-		print 'hi Twitter!'
 		if self.get_argument('oauth_token',None):
 			user = yield self.get_authenticated_user()
 			if not user:
 				self.clear_all_cookies()
 				raise tornado.web.HTTPError(500,'Twitter authentication failed')
-			self.set_secure_cookie('user_id',str(user['id']))
-			self.set_secure_cookie('oauth_token',user['access_token']['key'])
-			self.set_secure_cookie('oauth_secret',user['access_token']['secret'])
+			self.set_secure_cookie(self.COOKIE_NAME,json_encode(user))
 			self.redirect('/')
 		else:
 			yield self.authorize_redirect()
 
-
-class LogoutHandler(tornado.web.RequestHandler):
+class LogoutHandler(BaseHandler):
 	def get(self):
-		self.clear_all_cookies()
+		self.clear_cookie(self.COOKIE_NAME)
 		self.render('logout.html')
 
 
@@ -64,19 +73,13 @@ class Application(tornado.web.Application):
 			(r'/login',LoginHandler),
 			(r'/logout',LogoutHandler)
 		]
-		settings = {
-			# mask for security
-			'twitter_consumer_key':'Na3n........C5M0WIPND',
-			'twitter_consumer_secret':'3HW2TFHFls...................XrbqUMmIFcPzU93xa',
-			'cookie_secret':'NTliOTY5NzJkYTVlMTU0OTAwMTdlNjgzMTA5M2U3OGQ5NDIxZmU4Mg==',
-			'template_path': os.path.join(os.path.dirname(__file__),'templates'),
-			'debug':True,
-		}
-		tornado.web.Application.__init__(self,handlers,**settings)
+		tornado.web.Application.__init__(self,handlers,template_path=os.path.join(os.path.dirname(__file__),'templates'),login_url='/login',**options.group_dict('application'))
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
+	tornado.options.parse_config_file(options.config_file)
 	app = Application()
 	httpserver = tornado.httpserver.HTTPServer(app)
 	httpserver.listen(options.port)
+	logging.info('app run on the port: %d' % options.port )
 	tornado.ioloop.IOLoop.instance().start()
